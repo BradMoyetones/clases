edcdff9e2b9deaa72d25f3a77fe7969d07717b34
Brad Moyetones <brad.moyetones@gmail.com>
2025-11-15 21:27:35 -0500

    feat: Finish sidebar configuration and refactor nav header (Hide toggle sidebar)

--- WEB/clase-01/assets/scripts.js
+++ WEB/clase-01/assets/scripts.js
@@ -1,55 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
-    const open = document.getElementById("open")
-    const close = document.getElementById("close")
+let isOpen = false
+
+function toggleSidebar() { 
     const sidebar = document.getElementById("sidebar")
     const background = document.getElementById("sidebar-back")
-    let isOpen = false
-
-    open.addEventListener("click", function(){
-        if(!isOpen){
-            sidebar.style.transform = "translateX(0)";
-            background.style.opacity = "1";
-            background.style.pointerEvents = "all";
-
-            isOpen = true
-        }else {
-            sidebar.style.transform = "translateX(300px)";
-            background.style.opacity = "0";
-            background.style.pointerEvents = "none";
-            isOpen = false
-        }
-    })
-
-    close.addEventListener("click", function(){
-        if(!isOpen){
-            sidebar.style.transform = "translateX(0)";
-            background.style.opacity = "1";
-            background.style.pointerEvents = "all";
-
-            isOpen = true
-        }else {
-            sidebar.style.transform = "translateX(300px)";
-            background.style.opacity = "0";
-            background.style.pointerEvents = "none";
-
-            isOpen = false
-        }
-    })
-
-    background.addEventListener("click", function(){
-        if(!isOpen){
-            sidebar.style.transform = "translateX(0)";
-            background.style.opacity = "1";
-            background.style.pointerEvents = "all";
-
-            isOpen = true
-        }else {
-            sidebar.style.transform = "translateX(300px)";
-            background.style.opacity = "0";
-            background.style.pointerEvents = "none";
-
-            isOpen = false
-        }
-    })
-})
+    sidebar.style.transform = !isOpen ? "translateX(0)" : "translateX(300px)";
+    background.style.opacity = !isOpen ? "1" : "0";
+    background.style.pointerEvents = !isOpen ? "all" : "none";
+    isOpen = !isOpen ? true : false
+}
