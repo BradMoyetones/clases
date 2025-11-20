084b6832b84d702aad5ca2eba42a69d9262c52b7
BradMoyetones <brad.moyetones@gmail.com>
2025-11-20 08:19:06 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 import { createMDX } from "fumadocs-mdx/next"
-import path from "path";
 const withMDX = createMDX({
   // customise the config file path
   // configPath: "source.config.ts"
 });
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.join(__dirname, '..'),
-  },
   /* config options here */
   devIndicators: false,
   typescript: {
